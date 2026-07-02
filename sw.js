7074da27abb974073e853387cb3cf3e18b4fdb11
gab-loc19 <locontegabriele19@gmail.com>
2026-07-02 15:41:06 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kite-v29';
+const CACHE = 'kite-v30';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
